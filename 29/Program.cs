7c06691d6d10e16d0df8c2c24c5f858ea6e7e6dc
Mary <of51@mail.ru>
2023-02-12 20:46:09 +0300

    Домашка 4 по C#

---
+++ 29/Program.cs
@@ -0,0 +1,19 @@
+﻿//Напишите программу, которая задаёт массив из 8 элементов и
+// выводит их на экран.
+//1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+//6, 1, 33 -> [6, 1, 33]
+
+Console.Clear();
+int[] array = GetArray();
+Console.WriteLine($"Массив[{String.Join(",", array)}]");
+
+int[] GetArray(){
+    int[] array = new int[8];
+    for (int i = 1; i <= 8; i++)
+    {
+        Console.Write($"Введите {i} число массива: ");
+        int num = int.Parse(Console.ReadLine());
+        array[i-1] = num;
+    }
+    return array;
+}
